e4d120f534f62c47681c56a21edd2b0bd9b9c43f
Ephraim Debel <ephraimdebel@gmail.com>
2025-09-07 17:34:08 +0300

    bug fix

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -20,7 +20,7 @@ def stream_users_in_batches(batch_size):
         batch = cursor.fetchall()
         if not batch:
             break
-        yield batch
+        yield batch  # <-- use yield, not return
         offset += batch_size
 
     cursor.close()
@@ -29,7 +29,7 @@ def stream_users_in_batches(batch_size):
 
 def batch_processing(batch_size):
     """Process each batch and filter users over the age of 25"""
-    for batch in stream_users_in_batches(batch_size):  # 1st loop
-        for user in batch:  # 2nd loop
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
             if user['age'] > 25:
-                print(user)  # output or process user
+                yield user  # <-- also yield here
