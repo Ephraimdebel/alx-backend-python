a1e2eac4d2b44f5f7309059c1fa9e750a676f22f
Ephraim Debel <ephraimdebel@gmail.com>
2025-09-28 13:18:08 +0300

    bug fix

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets, filters, status
 from rest_framework.response import Response
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
@@ -21,11 +20,14 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all().select_related("sender", "conversation")
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsConversationParticipant]
     filter_backends = [filters.SearchFilter]
     search_fields = ["message_body"]
+
+    def get_queryset(self):
+        # Filter messages to only those in conversations the user participates in
+        return Message.objects.filter(conversation__participants=self.request.user)
 
     def create(self, request, *args, **kwargs):
         conversation_id = request.data.get("conversation")
