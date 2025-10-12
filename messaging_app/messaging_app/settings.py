9a05d1f54218fae80b594ad2749692fcdb5c99cb
Ephraim Debel <ephraimdebel@gmail.com>
2025-10-12 23:14:52 +0300

    docker compose added

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -97,10 +97,10 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DATABASE', 'messaging_db'),
+        'NAME': os.getenv('MYSQL_DB', 'messaging_db'),
         'USER': os.getenv('MYSQL_USER', 'messaging_user'),
         'PASSWORD': os.getenv('MYSQL_PASSWORD', 'StrongPassword123'),
-        'HOST': 'db',  # matches service name in docker-compose
+        'HOST': 'db',
         'PORT': '3306',
     }
 }
